4148f536b8caa7e32bc188dd8060eb4cb1a361b9
Daniel Brown <2469283B@student.gla.ac.uk>
2021-04-04 01:14:26 +0100

    genre.html now shows interests

--- matemaker/templatetags/matemaker_template_tags.py
+++ matemaker/templatetags/matemaker_template_tags.py
@@ -1,7 +1,14 @@
 from django import template
-from matemaker.models import Genre
+from matemaker.models import Genre, Interest
 register = template.Library()
 
 @register.inclusion_tag('matemaker/genrelist.html')
 def get_genre_list():
     return {'genres': Genre.objects.all()}
+
+@register.inclusion_tag('matemaker/interestlist.html')
+def get_interest_list(current_genre):
+    genre = Genre.objects.get(name=current_genre)
+
+    interests = Interest.objects.filter(genre=genre)
+    return {'interests': interests}
