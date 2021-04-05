6882a9337b9e3e1dadf0c8c6d8509d51f5aab2e6
Daniel Brown <2469283B@student.gla.ac.uk>
2021-04-05 05:53:45 +0100

    interests now have member list

--- matemaker/templatetags/matemaker_template_tags.py
+++ matemaker/templatetags/matemaker_template_tags.py
@@ -1,5 +1,5 @@
 from django import template
-from matemaker.models import Genre, Interest
+from matemaker.models import Genre, Interest, UserProfile
 register = template.Library()
 
 @register.inclusion_tag('matemaker/genrelist.html')
@@ -8,7 +8,15 @@ def get_genre_list():
 
 @register.inclusion_tag('matemaker/interestlist.html')
 def get_interest_list(current_genre):
-    genre = Genre.objects.get(name=current_genre)
+    genre = Genre.objects.get(slug=current_genre)
 
     interests = Interest.objects.filter(genre=genre)
     return {'interests': interests}
+
+@register.inclusion_tag('matemaker/memberlist.html')
+def get_member_list(current_interest):
+    print(current_interest)
+    interest = Interest.objects.get(name=current_interest)
+
+    members = UserProfile.objects.filter(intersts=interest)
+    return {'members': members}
